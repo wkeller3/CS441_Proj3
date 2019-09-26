649cd80cfeb4c125593578b9c3a1e11edca3e04f
Will Keller <wkeller3@binghamton.edu>
2019-09-26 16:11:27 -0400

    I now have a square that is affected by gravity and moves up when you touch the screen. I have no idea what I am going to do for this project

--- Project3/Project3/GameScene.swift
+++ Project3/Project3/GameScene.swift
@@ -11,23 +11,37 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var player: SKSpriteNode!
+    var player: SKSpriteNode!;
+    var floor: SKSpriteNode!;
     
     override func didMove(to view: SKView){
         createPlayer()
+        createSceneContents()
     }
     
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
-       
+        player.physicsBody?.applyForce(CGVector.init(dx:0,dy:4000));
     }
     
     func createPlayer(){
-        player = SKSpriteNode(color: UIColor.green, size: CGSize.init(width: 10, height: 10))
+        player = SKSpriteNode(color: UIColor.green, size: CGSize.init(width: 50, height: 50))
+        player.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
         player.zPosition = 10
-        player.position = CGPoint(x: frame.width/6, y: frame.height * 0.75)
+        player.position = CGPoint(x: frame.width/6, y: frame.height * 0.25)
+        player.physicsBody?.affectedByGravity = true
+        
+        //add textures and animate through them
+        //then repeatForever through them
+    
         addChild(player)
         
+    }
+
+    func createSceneContents() {
+        self.backgroundColor = .black
+        self.scaleMode = .aspectFit
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
     }
     
 }

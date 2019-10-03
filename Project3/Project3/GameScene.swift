2ce74c4a4bdd030f2cbb4956a17579793a07f2e6
Will Keller <wkeller3@binghamton.edu>
2019-10-03 16:10:23 -0400

    Still trying to figure out what to do. Made some progress I think

--- Project3/Project3/GameScene.swift
+++ Project3/Project3/GameScene.swift
@@ -12,21 +12,39 @@ import GameplayKit
 class GameScene: SKScene {
     
     var player: SKSpriteNode!;
+//    var player: SKShapeNode!;
     var floor: SKSpriteNode!;
+    var flag:Bool = false;
+    let cam = SKCameraNode();
     
     override func didMove(to view: SKView){
-        createPlayer()
-        createSceneContents()
-        createGround()
+        createPlayer();
+        createSceneContents();
+        createGround();
+        self.camera = cam;
     }
-    
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
         //player.physicsBody?.applyImpulse(CGVector.init(dx:0,dy:50));
-        player.physicsBody?.applyImpulse(CGVector.init(dx:50,dy:0));
+        flag = true;
+        //player.physicsBody?.applyImpulse(CGVector.init(dx:50,dy:0));
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?){
+        flag = false;
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        if(flag){
+            player.physicsBody?.applyImpulse(CGVector.init(dx:5,dy:0));
+        }
+        cam.position = CGPoint(x: player.position.x + 200, y: player.position.y);
     }
     
     func createPlayer(){
+//        player = SKShapeNode(circleOfRadius: 25);
+//        player.fillColor = (UIColor.green);
+//        player.physicsBody = SKPhysicsBody(circleOfRadius: 25);
         player = SKSpriteNode(color: UIColor.green, size: CGSize.init(width: 50, height: 50));
         player.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50));
         player.zPosition = 10;
@@ -43,7 +61,7 @@ class GameScene: SKScene {
     func createSceneContents() {
         self.backgroundColor = .white;
         self.scaleMode = .aspectFit;
-        self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame);
+        //self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame);
     }
     
     func createGround(){
@@ -51,7 +69,7 @@ class GameScene: SKScene {
 //        ground.position = CGPoint(x: 450, y: 0);
 //        ground.zPosition = -10;
 //        ground.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 900, height: 20));
-        let groundTexture = SKTexture(imageNamed: "ground1");
+        let groundTexture = SKTexture(imageNamed: "ground2");
         let ground = SKSpriteNode(texture: groundTexture);
         ground.position = CGPoint(x: groundTexture.size().width/2.0, y: groundTexture.size().height/2);
         ground.physicsBody = SKPhysicsBody(texture: groundTexture, size: groundTexture.size());
